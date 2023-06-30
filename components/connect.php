1595c66fe63739b5dee10a6a11f25f110935e1e5
Trần Khánh Dư <61740494+trankhanhdu54@users.noreply.github.com>
2023-06-30 16:41:53 +0700

    Update connect.php

--- components/connect.php
+++ components/connect.php
@@ -3,9 +3,9 @@
 
 
 
-$db_name = 'mysql:host=localhost;dbname=trandugold';
-$user_name = 'root';
-$user_password = '';
+$db_name = 'mysql:host=bln4gido9kwljjn4et1g-mysql.services.clever-cloud.com:3306;dbname=bln4gido9kwljjn4et1g';
+$user_name = 'ujktaoitnxiu30jv';
+$user_password = '9lTnWoISxsAEFKVBHUwS';
 date_default_timezone_set('Asia/Ho_Chi_Minh'); 
 $conn = new PDO($db_name, $user_name, $user_password);
 
